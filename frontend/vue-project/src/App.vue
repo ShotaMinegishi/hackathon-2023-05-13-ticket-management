4e231f29fcf12ca8d385704e474eea78bfaeb96d
Yurie Itakura <itakura.contact@gmail.com>
2023-09-16 12:43:30 +0900

    vueファイルアップロード

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -1,85 +1,206 @@
 <script setup>
+import { ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+const modalDialog = ref(null);
+
+// タスクを追加する関数
+const addTask = () => {
+   if (newTask.value.name) {
+      taskList.value.push({ ...newTask.value });
+      newTask.value = {
+         name: '',
+         deadline: '',
+         startTime: '',
+         endTime: '',
+         details: ''
+      };
+   }
+   if (modalDialog.value) {
+      modalDialog.value.classList.remove('open');
+   }
+};
+
+// タスクを削除する関数
+const removeTask = (index) => {
+   taskList.value.splice(index, 1);
+};
+
+// モーダル開閉の関数
+const openModal = () => {
+   if (modalDialog.value) {
+      modalDialog.value.classList.add('open');
+   }
+};
+
+const closeModal = () => {
+   if (modalDialog.value) {
+      modalDialog.value.classList.remove('open');
+   }
+};
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <nav>
+      <h1><router-link to="/">TASK MANAGER</router-link></h1>
+      <div class="bell alert">
+        <img alt="通知" src="@/assets/bell.svg" width="25" height="25">
+      </div>
+    </nav>
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
+       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
+  <main>
+      <h2 class="date"><span>2023</span>09/09</h2>
+      <ul>
+         <li class="task-item">
+            <span class="check checked"></span>
+            <div>
+               <p>ここにタスクが入ります。ここにタスクが入ります。ここにタスクが入ります。</p>
+               <div class="bottom">
+                  <span class="time">8:00 - 11:00</span>
+                  <button class="edit">編集</button>
+               </div>
+            </div>
+         </li>
+         <li class="task-item">
+            <span class="check"></span>
+            <div>
+               <p>ここにタスクが入ります。ここにタスクが入ります。ここにタスクが入ります。</p>
+               <div class="bottom">
+                  <span class="time">8:00 - 11:00</span>
+                  <button class="edit">編集</button>
+               </div>
+            </div>
+         </li>
+         <li class="task-item">
+            <span class="check"></span>
+            <div class="inner">
+               <p>ここにタスクが入ります。ここにタスクが入ります。ここにタスクが入ります。</p>
+               <div class="bottom">
+                  <span class="time">8:00 - 11:00</span>
+                  <button class="edit">編集</button>
+               </div>
+            </div>
+         </li>
+      </ul>
+      <h2 class="date"><span>2023</span>09/15</h2>
+      <ul id="task">
+         <li class="task-item" v-for="(task, index) in taskList" :key="index">
+            <span class="check"></span>
+            <div class="inner">
+               <p>{{ task.name }}</p>
+               <div class="bottom">
+                  <div>{{ task.deadline }}</div>
+                  <div class="time">{{ task.startTime }} - {{ task.endTime }}</div>
+                  <!-- <button class="edit">編集</button> -->
+                  <button @click="removeTask(index)" class="edit">削除</button>
+               </div>
+            </div>
+         </li>
+      </ul>
+   </main>
+   <button class="btn-plus" @click="openModal()"></button>
 
+   <div class="modal" ref="modalDialog">
+      <button class="btn-close" @click="closeModal()"></button>
+      <label id="task">
+         <input type="text" v-model="newTask.name" placeholder="タスクを追加してください" calus>
+      </label>
+      <label id="limit">
+         <!-- <span class="icon"></span> -->
+         <p>期限</p>
+         <input type="date" v-model="newTask.deadline">
+      </label>
+      <label id="startTime">
+         <!-- <span class="icon"></span> -->
+         <p>開始時間</p>
+         <input type="time" v-model="newTask.startTime">
+      </label>
+      <label id="endTime">
+         <!-- <span class="icon"></span> -->
+         <p>終了時間</p>
+         <input type="time" v-model="newTask.endTime">
+      </label>
+      <label id="desc">
+         <!-- <span class="icon"></span> -->
+         <p>タスクの詳細</p>
+         <textarea rows="7" placeholder="タスクの詳細を入力してください"></textarea>
+      </label>
+      <button class="btn-add" @click="addTask">タスクを追加</button>
+   </div>
   <RouterView />
 </template>
 
+
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+   padding: 1em;
+   text-align: center;
+   line-height: 1.5;
+   max-height: 100vh;
+   border-bottom: 1px solid #000;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+   font-size: 14px;
+   font-weight: 600;
+   width: 100%;
+   position: relative;
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+   color: var(--color-text);
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: transparent;
+   background-color: transparent;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+   display: inline-block;
+   padding: 0 1rem;
+   border-left: 1px solid var(--color-border);
 }
 
 nav a:first-of-type {
-  border: 0;
+   border: 0;
 }
 
+main {
+   padding: 50px 5%;
+}
+
+.bottom div {
+   margin-top: 10px;
+}
+
+
+
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+   .logo {
+      margin: 0 2rem 0 0;
+   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+   header .wrapper {
+      display: flex;
+      place-items: flex-start;
+      flex-wrap: wrap;
+   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+   nav {
+      text-align: left;
+      margin-left: -1rem;
+      font-size: 1rem;
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+      padding: 1rem 0;
+      margin-top: 1rem;
+   }
 }
 </style>
---
+++ frontend/vue-project/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+   <header>
+      <nav>
+         <h1><router-link to="/">TASK MANAGER</router-link></h1>
+      </nav>
+   </header>
+</template>
+ 
+<script>
+export default {
+   name: 'Header',
+}
+</script>
